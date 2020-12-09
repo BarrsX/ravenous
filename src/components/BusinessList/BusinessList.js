b24c93066840d6d9ea5eca92cb9969a4ec740d72
Xavion Barrs <xavionbarrs@gmail.com>
2020-12-09 17:29:01 -0500

    Added support for Yelp API.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import Business from '../Business/Business';
 import './BusinessList.css';
-import Business from '../Business/Business.js';
 
 class BusinessList extends React.Component {
-  render() {
-    return (
-      <div className="BusinessList">
-        {
-          this.props.businesses.map((business) => {
-            return <Business business={business} />;
-          })
-        }
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="BusinessList">
+                {
+                    this.props.businesses.map(function(business, i) {
+                        return <Business key={business.id} business={business} />;
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default BusinessList;
---
+++ src/util/Yelp.js
@@ -0,0 +1,37 @@
+const apiKey = 'Asq_VxuCrf65QIu2D7u0jn8WYCdpcG6FnVzZ-tqaqBaAuVdqsYSRS7eDk9oCOXTo0_drrskKyj3l85HkzD2yp8Va95Ex4SDu1sbD5618saKwBtfntULoKwsmyU7RX3Yx'
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        }
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map(business => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count
+            };
+          });
+        }
+      });
+  }
+};
+
+export default Yelp;
