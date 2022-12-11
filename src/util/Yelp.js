7ccf77ec75512d07ed9fd59072bf575f4283b938
barrsx <xavionbarrs@gmail.com>
2022-12-11 12:16:14 -0500

    cors fix

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -3,7 +3,7 @@ const apiKey = 'sP5eSul4f480F-oNSWCkmvnp2IlDFR-r2j9JJQHGRYFBI9S8im_raIsnlli7eBNx
 const Yelp = {
   search(term, location, sortBy) {
     return fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      `https://morning-stream-08762.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
       {
         headers: {
           Authorization: `Bearer ${apiKey}`
