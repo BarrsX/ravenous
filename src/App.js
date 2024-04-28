80a200fcc6282055284bceab8b2eae702a965119
BarrsX <xavionbarrs@gmail.com>
2024-04-28 09:39:06 -0400

    Refactors CSS styles and updates BusinessList component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import BusinessList from './components/BusinessList/BusinessList';
-import SearchBar from './components/SearchBar/SearchBar';
-import Yelp from './util/Yelp';
-import './App.css';
+import React from "react";
+import BusinessList from "./components/BusinessList/BusinessList";
+import SearchBar from "./components/SearchBar/SearchBar";
+import Yelp from "./util/Yelp";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      'businesses': []
-    }
+      businesses: [],
+      searchPerformed: false,
+    };
 
     this.searchYelp = this.searchYelp.bind(this);
   }
   searchYelp(term, location, sortBy) {
-    Yelp.search(term, location, sortBy).then(businesses => {
+    Yelp.search(term, location, sortBy).then((businesses) => {
       this.setState({
-        'businesses': businesses
+        businesses: businesses,
+        searchPerformed: true,
       });
     });
   }
@@ -26,7 +28,10 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={this.state.businesses} />
+        <BusinessList
+          businesses={this.state.businesses}
+          searchPerformed={this.state.searchPerformed}
+        />
       </div>
     );
   }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import Business from '../Business/Business';
-import './BusinessList.css';
+import React from "react";
+import Business from "../Business/Business";
+import "./BusinessList.css";
 
 class BusinessList extends React.Component {
-    render() {
-        return (
-            <div className='BusinessList'>
-                {this.props.businesses.map(business => {
-                    return <Business business={business} key={business.id}/>
-                })}
-            </div>
-        )
+  render() {
+    if (!this.props.searchPerformed) {
+      return (
+        <div className="BusinessList BusinessList-empty">
+          <p>
+            Please enter a type of food and location to get recommendations.
+          </p>
+        </div>
+      );
+    } else if (this.props.businesses.length === 0) {
+      return (
+        <div className="BusinessList BusinessList-empty">
+          <p>No businesses found. Please try another search.</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="BusinessList">
+          {this.props.businesses.map((business) => {
+            return <Business business={business} key={business.id} />;
+          })}
+        </div>
+      );
     }
+  }
 }
 
 export default BusinessList;
