2a33974dbc30931aa90f2d678fa1097c5e39cb06
BarrsX <xavionbarrs@gmail.com>
2024-04-28 10:15:10 -0400

    Adds LoadingBar component and updates BusinessList to display loading animation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import BusinessList from "./components/BusinessList/BusinessList";
 import SearchBar from "./components/SearchBar/SearchBar";
 import Yelp from "./util/Yelp";
 import "./App.css";
+import "./LoadingBar.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,15 +11,19 @@ class App extends React.Component {
     this.state = {
       businesses: [],
       searchPerformed: false,
+      isLoading: false,
     };
 
     this.searchYelp = this.searchYelp.bind(this);
   }
+
   searchYelp(term, location, sortBy) {
+    this.setState({ isLoading: true });
     Yelp.search(term, location, sortBy).then((businesses) => {
       this.setState({
         businesses: businesses,
         searchPerformed: true,
+        isLoading: false,
       });
     });
   }
@@ -31,6 +36,7 @@ class App extends React.Component {
         <BusinessList
           businesses={this.state.businesses}
           searchPerformed={this.state.searchPerformed}
+          isLoading={this.state.isLoading}
         />
       </div>
     );
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,32 +1,31 @@
 import React from "react";
 import Business from "../Business/Business";
 import "./BusinessList.css";
+import "../../LoadingBar.css";
 
 class BusinessList extends React.Component {
   render() {
-    if (!this.props.searchPerformed) {
-      return (
-        <div className="BusinessList BusinessList-empty">
-          <p>
-            Please enter a type of food and location to get recommendations.
-          </p>
-        </div>
-      );
-    } else if (this.props.businesses.length === 0) {
-      return (
-        <div className="BusinessList BusinessList-empty">
-          <p>No businesses found. Please try another search.</p>
-        </div>
-      );
-    } else {
-      return (
-        <div className="BusinessList">
-          {this.props.businesses.map((business) => {
+    return (
+      <div className="BusinessList">
+        {this.props.isLoading ? (
+          <div className="LoadingBar"></div>
+        ) : !this.props.searchPerformed ? (
+          <div className="BusinessList-empty">
+            <p>
+              Please enter a type of food and location to get recommendations.
+            </p>
+          </div>
+        ) : this.props.businesses.length === 0 ? (
+          <div className="BusinessList-empty">
+            <p>No businesses found. Please try another search.</p>
+          </div>
+        ) : (
+          this.props.businesses.map((business) => {
             return <Business business={business} key={business.id} />;
-          })}
-        </div>
-      );
-    }
+          })
+        )}
+      </div>
+    );
   }
 }
 
