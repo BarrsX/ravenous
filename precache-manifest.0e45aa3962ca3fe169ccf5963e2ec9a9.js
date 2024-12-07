8f83480fe3b31ff10cc97f015c818274f55ac86f
Xavion Barrs <xavionbarrs@gmail.com>
2024-12-07 13:01:00 -0500

    Updates

---
+++ precache-manifest.0e45aa3962ca3fe169ccf5963e2ec9a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c76f9ef5ff0d636efae78642ccd3555",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "0345652300104552e141",
+    "url": "/ravenous/static/css/main.ef517a55.chunk.css"
+  },
+  {
+    "revision": "d5e6f1e7eb8afb8d5f09",
+    "url": "/ravenous/static/js/2.75d1f382.chunk.js"
+  },
+  {
+    "revision": "3c3833cc90e51ae822f01e296c687ebb",
+    "url": "/ravenous/static/js/2.75d1f382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0345652300104552e141",
+    "url": "/ravenous/static/js/main.72e58791.chunk.js"
+  },
+  {
+    "revision": "755ecb392b0f1b1857f8",
+    "url": "/ravenous/static/js/runtime-main.0ebcc034.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
